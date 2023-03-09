18a603905cfe8142644640426fdc46d5c051eeb1
Bo Nicolajsen <squirrelworks@users.noreply.github.com>
2023-03-09 14:17:31 +0100

    vis mickey

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,58 @@ const myAppElement = document.getElementById('myApp');
 loadingScreen();
 setUpShowAllButton();
 setupSearchForm();
+fetchOneCharacter(4703);
+
+
+
+function fetchOneCharacter(myId) {
+    let URI = `https://api.disneyapi.dev/characters/${myId}`
+
+    fetch(URI).then((response) => {
+        //console.log(response);
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("api error du får lige mickey mouse");
+            fetchOneCharacter(4703);
+
+        }
+
+
+
+    }).then((data) => {
+
+        //console.log(data);
+        showCharacter(data);
+
+    }).catch();
+
+}
+
+function showCharacter(myData) {
+    //  myAppElement
+    console.log(myData.name);
+
+    let myFilms = '<h3>Films:</h3>';
+    myData.films.map((film) => {
+        myFilms += `${film}, `
+    }
+    );
+
+
+    let myTvShows = '<h3>TV Shows:</h3>';
+    myData.tvShows.map((show) => {
+        myTvShows += `${show}, `
+    }
+    );
+
+
+
+    let myHTML = `<h2>${myData.name}</h2><img src="${myData.imageUrl}"><p>${myFilms}</p><p>${myTvShows}</p>`;
+    myAppElement.innerHTML = myHTML;
+
+}
 
 // loading screen  kaldes når vi henter data
 function loadingScreen() {
@@ -18,7 +70,7 @@ function setUpShowAllButton() {
     let showAllButton = document.getElementById('showAllButton');
     showAllButton.addEventListener('click', (e) => {
         myPage = 1;
-        fetchCaracterPage();
+        fetchAllCharacters();
     });
 }
 
